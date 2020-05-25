39f808533142477cb5a065cee37412bb53a6bed8
Max Keeble <meckeeble@hotmail.co.uk>
2020-05-25 18:11:46 +0100

    Add files via upload
    "PlayerLook.cs" courtesy of a Youtube tutorial. I'll add credit when I get round to it,

---
+++ SaveBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 startPos;
+    public Rigidbody rb;
+    void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -2){
+            transform.position = startPos;
+            rb.velocity = new Vector3(0f,0f,0f);
+        }
+    }
+}
---
+++ win.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class win : MonoBehaviour
+{
+    public Text winText;
+    public GameObject board;
+    public MeshRenderer ball;
+    public MeshRenderer quad;
+    public MeshRenderer [] boardPieces;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        boardPieces = board.GetComponentsInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void OnTriggerEnter(Collider c){
+        winText.enabled = true;
+        foreach(MeshRenderer m in boardPieces){
+            m.enabled = false;
+        }
+        ball.enabled = false;
+        quad.enabled = false;
+    }
+}
